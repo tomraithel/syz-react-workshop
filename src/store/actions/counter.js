c529bbd111b4bbbd74c80942e62a46e5d4a0f625
Thomas Raithel <T.Raithel@syzygy.de>
2019-03-21 14:14:28 +0100

    Use new API_BASE_URL

--- src/store/actions/counter.js
+++ src/store/actions/counter.js
@@ -1,3 +1,5 @@
+import { API_BASE_URL } from "../../lib/api";
+
 // Constants
 export const INC = "COUNTER_INC";
 export const RESET = "COUNTER_RESET";
@@ -9,7 +11,7 @@ export const reset = () => ({ type: RESET });
 export const set = number => ({ type: SET, payload: number });
 export const setRandom = () => dispatch => {
   dispatch(set("???"));
-  fetch("http://68.183.212.209:8080/random-number")
+  fetch(`${API_BASE_URL}/random-number`)
     .then(response => response.json())
     .then(data => {
       dispatch(set(data.number));
